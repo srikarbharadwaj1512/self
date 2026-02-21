fa6d76202d1fea7549efbdba64ada672a591cf00
Srikar Bharadwaj T S <srikarbharadwajts@Srikars-MacBook-Air.local>
2026-02-21 03:57:54 -0800

    conflict commit

--- self.py
+++ self.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-
-=======
 print("Learning GIT!")
 print("this is from Nikhil")
 print("second line")
 print("Third line")
 print(This is fourth line)
->>>>>>> 148fab5 (conflict commit)
